62b9ff3015a49cd52f36f0d656be19a75f805218
Afonso Soares <afsoares.tec@outlook.com>
2025-04-15 19:17:20 +0100

    Update Project

--- DonkeyKong_122715_AfonsoSoares_123280_RodrigoSantos_2024_2025/src/pt/iscte/poo/game/Main.java
+++ DonkeyKong_122715_AfonsoSoares_123280_RodrigoSantos_2024_2025/src/pt/iscte/poo/game/Main.java
@@ -6,14 +6,7 @@ import pt.iscte.poo.gui.ImageGUI;
 
 public class Main {
 
-	private static final String IMAGE_DIR = "images";
-
 	public static void main(String[] args) {
-		File folder = new File(IMAGE_DIR);
-        System.out.println("Pasta: " + folder.getAbsolutePath());
-        System.out.println("Existe? " + folder.exists());
-        System.out.println("É diretório? " + folder.isDirectory());
-
 		ImageGUI gui = ImageGUI.getInstance();
 		GameEngine engine = new GameEngine();
 		gui.setStatusMessage("Good luck!");
